fe52ff08759243c5510721bf5f853bce9df8b115
Adithya-Sahyadri-ECE <adithyarg14@gmail.com>
2025-04-02 23:26:01 +0530

    Update mux_4to1.v

--- Day_2/src/mux_4to1.v
+++ Day_2/src/mux_4to1.v
@@ -1,1 +1,11 @@
+module mux_4to1(
+    input wire A, B, C, D,
+    input wire [1:0] Sel,
+    output wire Out
+);
 
+assign Out = (Sel == 2'b00) ? A :
+             (Sel == 2'b01) ? B :
+             (Sel == 2'b10) ? C : D;
+
+endmodule
