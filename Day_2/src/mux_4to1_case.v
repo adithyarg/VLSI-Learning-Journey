f3463419fd35d1bfe823508d6948032a37dbbeeb
Adithya-Sahyadri-ECE <adithyarg14@gmail.com>
2025-04-02 23:26:12 +0530

    Update mux_4to1_case.v

--- Day_2/src/mux_4to1_case.v
+++ Day_2/src/mux_4to1_case.v
@@ -1,1 +1,16 @@
+module mux_4to1_case(
+    input wire A, B, C, D,
+    input wire [1:0] Sel,
+    output reg Out
+);
+
+always @(*) begin
+    case (Sel)
+        2'b00: Out = A;
+        2'b01: Out = B;
+        2'b10: Out = C;
+        2'b11: Out = D;
+        default: Out = 0;
+    endcase
+end
 
