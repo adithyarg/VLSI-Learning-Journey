e450309692ff14bc0916f92645b39aa75c7e75a5
Adithya-Sahyadri-ECE <adithyarg14@gmail.com>
2025-04-02 23:25:33 +0530

    Update mux_2to1.v

--- Day_2/src/mux_2to1.v
+++ Day_2/src/mux_2to1.v
@@ -1,1 +1,9 @@
+module mux_2to1(
+    input wire A, B,   // Data inputs
+    input wire Sel,    // Select signal
+    output wire Out    // Output
+);
 
+assign Out = (Sel == 1'b0) ? A : B;
+
+endmodule
