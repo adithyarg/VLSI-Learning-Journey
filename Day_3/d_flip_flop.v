332448274c0ce352c520c3adb0aa2da99a2e3da5
Adithya-Sahyadri-ECE <adithyarg14@gmail.com>
2025-04-03 22:49:51 +0530

    Update d_flip_flop.v

--- Day_3/d_flip_flop.v
+++ Day_3/d_flip_flop.v
@@ -1,1 +1,13 @@
-
+module d_flip_flop (
+    input CLK,
+    input D,
+    input RESET,
+    output reg Q
+);
+    always @(posedge CLK or posedge RESET) begin
+        if (RESET)
+            Q <= 0;
+        else
+            Q <= D;
+    end
+endmodule
