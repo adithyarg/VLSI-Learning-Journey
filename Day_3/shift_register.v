1c9fbb3bfda9c99204bdb886c087844bf9b2ec67
Adithya-Sahyadri-ECE <adithyarg14@gmail.com>
2025-04-03 22:50:42 +0530

    Update shift_register.v

--- Day_3/shift_register.v
+++ Day_3/shift_register.v
@@ -1,1 +1,13 @@
-
+module shift_register (
+    input CLK,
+    input D,
+    input RESET,
+    output reg [3:0] Q
+);
+    always @(posedge CLK or posedge RESET) begin
+        if (RESET)
+            Q <= 4'b0000;
+        else
+            Q <= {Q[2:0], D};
+    end
+endmodule
