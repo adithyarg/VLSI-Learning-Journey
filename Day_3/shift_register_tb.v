9349597d40d8a6a9f799460c0a68749b12e43737
Adithya-Sahyadri-ECE <adithyarg14@gmail.com>
2025-04-03 22:51:00 +0530

    Update shift_register_tb.v

--- Day_3/shift_register_tb.v
+++ Day_3/shift_register_tb.v
@@ -1,1 +1,31 @@
+`timescale 1ns/1ps
 
+module shift_register_tb;
+    reg CLK, D, RESET;
+    wire [3:0] Q;
+
+    shift_register uut (
+        .CLK(CLK),
+        .D(D),
+        .RESET(RESET),
+        .Q(Q)
+    );
+
+    always #5 CLK = ~CLK;
+
+    initial begin
+        CLK = 0; D = 0; RESET = 1;
+        #10 RESET = 0;
+        #10 D = 1;
+        #10 D = 0;
+        #10 D = 1;
+        #10 D = 1;
+        #10 D = 0;
+        #50;
+        $stop;
+    end
+
+    initial begin
+        $monitor("Time: %0dns, CLK: %b, D: %b, RESET: %b, Q: %b", $time, CLK, D, RESET, Q);
+    end
+endmodule
